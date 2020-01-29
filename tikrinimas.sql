e4d8ea09d7850974152c38f28f9e02215c9e8274
toma_sudo <toma.neniskyte@gmail.com>
2020-01-29 14:11:46 +0200

    3_dienis

--- tikrinimas.sql
+++ tikrinimas.sql
@@ -1,4 +1,11 @@
-INSERT INTO `tikrinimo_lentele`(`id_prekes`,`id_tiekejo`,`id_prekiu_grupes`,`barkodas`,`nuo_datos`)
+INSERT INTO 
+	`tikrinimo_lentele`(
+		`id_prekes`
+		,`id_tiekejo`
+		,`id_prekiu_grupes`
+		,`barkodas`
+		,`nuo_datos`
+	)
 SELECT 
     	`prekes`.`id` AS `id_prekes`
 	, `prekes_tiekejai`.`id_tiekejo` AS `id_tiekejo`
@@ -11,3 +18,38 @@ LEFT JOIN
 	`prekes_tiekejai` ON( 
         `prekes_tiekejai`.`id_prekes` = `prekes`.`id`
     ) 
+
+
+SELECT 
+    	`prekes_tiekejai`.`id_prekes` AS `id_prekes`
+	, `prekes_tiekejai`.`barkodas` AS `barkodas`
+    	, `prekes_gavimai`.`id` AS `id_prekes_gavimo`
+    	, SUM(`prekes_pardavimai`.`kiekis`) AS `kiekis_viso`
+	, SUM(`prekes_pardavimai`.`vnt_kaina`*`prekes_pardavimai`.`kiekis`) AS `viso_suma`
+FROM 
+	`prekes_tiekejai`
+LEFT JOIN 
+	`prekes_gavimai` ON( 
+       	
+		`prekes_tiekejai`.`barkodas`= `prekes_gavimai`.`barkodas`
+    	)
+LEFT JOIN 
+	`prekes_pardavimai` ON(
+		`prekes_gavimai`.`id` = `prekes_pardavimai`.`id_prekes_gavimo`
+	)
+WHERE
+	 1
+GROUP BY 
+	`id_prekes`
+
+
+SELECT 
+	`id_prekes`
+	, SUM(`kiekis_parduota`) AS `kiekis_viso`
+	, SUM(`suma_parduota`) AS `suma_viso`
+FROM 
+	`balanso_lentele` 
+WHERE 
+	1 
+GROUP BY 
+	`id_prekes`
